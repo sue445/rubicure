f61ce828417e02cdf889dfd830d5ae114a38c4e7
sue445 <sue445@sue445.net>
2013-12-01 16:14:37 +0900

    Write method doc

--- lib/rubicure/series.rb
+++ lib/rubicure/series.rb
@@ -2,15 +2,19 @@ module Rubicure
   require 'yaml'
 
   class Series
+    # @return [Array<Symbol>]
     def self.names
       Series.config.keys
     end
 
+    # @return [Hash]
     def self.config
       config_file = "#{File.dirname(__FILE__)}/../../config/series.yml"
       YAML.load_file(config_file).deep_symbolize_keys
     end
 
+    # @param series_name [Symbol]
+    # @return [Hash]
     def self.fetch(series_name)
       Series.config[series_name]
     end
