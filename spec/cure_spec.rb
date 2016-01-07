9b1dc83a3054b2b3206c575b0765d45e10b0a9c0
sue445 <sue445@sue445.net>
2016-01-08 00:15:49 +0900

    Add twilight test

--- spec/cure_spec.rb
+++ spec/cure_spec.rb
@@ -84,4 +84,37 @@ describe "Cure" do
       end
     end
   end
+
+  describe ".scarlet" do
+    describe "!" do
+      subject { !Cure.scarlet }
+
+      let(:girl) { Cure.scarlet }
+
+      after do
+        girl.rollback
+        girl.humanize!
+      end
+
+      context "called once" do
+        it { expect { subject }.to change { girl.name }.from("紅城トワ").to("トワイライト") }
+      end
+
+      context "called twice" do
+        before do
+          !Cure.scarlet
+        end
+
+        it { expect { subject }.to change { girl.name }.from("トワイライト").to("紅城トワ") }
+      end
+
+      context "after transform" do
+        before do
+          girl.transform!
+        end
+
+        it { expect { subject }.to change { girl.name }.from("キュアスカーレット").to("トワイライト") }
+      end
+    end
+  end
 end
