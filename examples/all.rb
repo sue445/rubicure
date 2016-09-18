389814724419e99d391bafed75beb3a7ce9cd070
sue445 <sue445@sue445.net>
2016-09-18 23:08:26 +0900

    Refactor: Move verbose spec to examples

---
+++ examples/all.rb
@@ -0,0 +1,25 @@
+require "rubicure"
+
+Precure.each_with_series do |series|
+  puts <<EOS
+====================
+title:     #{series.title}
+broadcast: #{series.started_date} - #{series.try(:ended_date)}
+girls:     #{series.girls.count}
+EOS
+
+  series.girls.each do |girl|
+    puts <<EOS
+------------------------
+  human_name:   #{girl.human_name}
+  precure_name: #{girl.precure_name}
+  cast_name:    #{girl.cast_name}
+  color:        #{girl.color}
+  extra_names:  #{girl[:extra_names]}
+  state_names:  #{girl.state_names}
+  attack_messages: #{girl.attack_messages}
+  transform_message:
+#{girl.transform_message}
+EOS
+  end
+end
