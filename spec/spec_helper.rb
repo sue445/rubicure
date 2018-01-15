f3413fdb725275703d56c4becfc5bfb05ef974bf
sue445 <sue445@users.noreply.github.com>
2018-01-15 22:43:08 +0900

    Refactor spec_helper :golf:

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,7 +39,7 @@ require "pry"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir["#{__dir__}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   # The settings below are suggested to provide a good initial experience
