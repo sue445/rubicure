89d2fc6ca3cf34dcb27ec96d2336715ce0839c35
sue445 <sue445@sue445.net>
2019-06-23 08:14:51 +0900

    to_date returns Time instead of Date when args is Time

--- lib/rubicure/concerns/util.rb
+++ lib/rubicure/concerns/util.rb
@@ -4,13 +4,14 @@ module Rubicure
     module Util
       # @param arg [Date,Time,String]
       #
-      # @return [Date] arg is String or Date
-      # @return [Time] arg is Time
+      # @return [Date] arg is String, Date or Time
       # @return [nil] arg is other
       def to_date(arg)
         case arg
-        when Date, Time
+        when Date
           arg
+        when Time
+          arg.to_date
         when String
           begin
             Date.parse(arg)
