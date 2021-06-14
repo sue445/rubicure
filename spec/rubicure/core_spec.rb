8c01cf767a62a5f7e852cc0035fe274c98cfb44a
sue445 <sue445@sue445.net>
2021-06-14 23:50:21 +0900

    Add CureLamer

--- spec/rubicure/core_spec.rb
+++ spec/rubicure/core_spec.rb
@@ -86,7 +86,7 @@ describe Rubicure::Core do
     context "Without arg" do
       subject { instance.all_girls }
 
-      let(:precure_count) { 69 }
+      let(:precure_count) { 70 }
 
       its(:count) { should == precure_count }
       it { should include Cure.echo }
