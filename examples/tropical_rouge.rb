d3c21587e58207fef445927f3686c5882b4dce39
sue445 <sue445@sue445.net>
2022-01-24 22:19:30 +0900

    Add examples/tropical_rouge.rb

---
+++ examples/tropical_rouge.rb
@@ -0,0 +1,18 @@
+require "rubicure"
+require_relative "./common"
+
+code = <<~RUBY
+  @manatsu = Cure.summer
+  puts @manatsu.name
+
+  @manatsu.precure_tropical_change!
+  puts @manatsu.name
+
+  @manatsu.humanize!
+  puts @manatsu.name
+
+  @manatsu.precure_tropical_change!
+  puts @manatsu.name
+RUBY
+
+run code
