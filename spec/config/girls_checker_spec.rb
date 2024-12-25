d0796fb2cf468247c00d2a9abea72acabc2191a0
sue445 <sue445@sue445.net>
2024-12-25 23:33:43 +0900

    Resolve Lint/RedundantCopDisableDirective

--- spec/config/girls_checker_spec.rb
+++ spec/config/girls_checker_spec.rb
@@ -43,11 +43,9 @@ describe "girls_checker" do # rubocop:disable RSpec/DescribeClass
             subject { transform_calls }
 
             transform_calls.count.times do |n|
-              # rubocop:disable RSpec/RepeatedDescription `expect().not_to all( matcher )` is not supported.
               its([n]) { should_not start_with "precure_" }
               its([n]) { should_not end_with "_precure" }
               its([n]) { should_not end_with "!" }
-              # rubocop:enable RSpec/RepeatedDescription
             end
           end
         end
