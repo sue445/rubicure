4f75693375b28c1085474ff38db1c127869cf88c
sue445 <sue445@sue445.net>
2025-06-15 10:16:18 +0900

    Add CureZukyoon :bow_and_arrow: and CureKiss :kiss:

--- spec/rubicure/core_spec.rb
+++ spec/rubicure/core_spec.rb
@@ -90,7 +90,7 @@ describe Rubicure::Core do
     context "Without arg" do
       subject { instance.all_girls }
 
-      let(:precure_count) { 86 }
+      let(:precure_count) { 88 }
 
       its(:count) { should == precure_count }
       it { should include Cure.echo }
